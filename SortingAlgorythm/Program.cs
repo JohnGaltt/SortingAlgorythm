8448d6c37817dd665b6e53598952536a3330c885
oleksandr.zholob <oleksandr.zholob@perfectial.com>
2018-10-24 17:50:10 +0300

    Init commit

---
+++ SortingAlgorythm/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SortingAlgorythm
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputList = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 1
+                },
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 5
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 10
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 20
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 30
+                }
+            };
+
+            var inputList1 = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 5
+                },
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 10
+                }
+            };
+
+            var newList = OrderHelper.Reorder(inputList1, null, 0, 1).OrderBy(x => x.OrderValue).ToArray();
+
+            Console.WriteLine("Test");
+            for (int i = 0; i < newList.Length; i++)
+            {
+                Console.WriteLine($"{newList[i].OrderValue} {newList[i].OrderName}");  
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class OrderItem
+    {
+        public double OrderValue { get; set; }
+
+        public string OrderName { get; set; }
+
+        public static bool operator > (OrderItem oldItem, OrderItem newItem)
+        {
+            if (oldItem.OrderValue > newItem.OrderValue)
+            {
+                return true;
+            }
+               
+            return false;
+        }
+
+        public static bool operator <(OrderItem oldItem, OrderItem newItem)
+        {
+            if (oldItem.OrderValue < newItem.OrderValue)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static double operator + (OrderItem oldItem, OrderItem newItem)
+        {
+            return oldItem.OrderValue + newItem.OrderValue;
+        }
+    }
+
+
+
+
+    public static class OrderHelper
+    {
+       public static OrderItem[] Reorder(IEnumerable<OrderItem> source, Expression<Func<OrderItem>> orderByPath, int oldPosition, int newPosition)
+       {
+           double newOrderValue = 0;
+            var orderItems = source as OrderItem[] ?? source.ToArray();
+            if (orderItems[oldPosition] > orderItems[newPosition])
+            {
+                if (newPosition == 0)
+                {
+                    newOrderValue = 5;
+                    orderItems[oldPosition].OrderValue = newOrderValue;
+                }
+                else
+                {
+                    newOrderValue = Math.Ceiling((orderItems[newPosition] + orderItems[newPosition - 1]) / 2);
+                    orderItems[oldPosition].OrderValue = newOrderValue;
+                }
+
+                if (newOrderValue == orderItems[newPosition].OrderValue)
+                {
+                    orderItems[newPosition].OrderValue++;
+                    while (orderItems[newPosition + 1].OrderValue == orderItems[newPosition].OrderValue)
+                    {
+                        orderItems[newPosition + 1].OrderValue++;
+                        newPosition++;
+                    }
+                }
+               
+            } else if (orderItems[oldPosition] < orderItems[newPosition]) {
+
+                //end
+                if (newPosition == orderItems.Length - 1)
+                {
+                    orderItems[oldPosition].OrderValue = orderItems[newPosition].OrderValue + 1;
+                }
+                else
+                {
+                    newOrderValue = Math.Ceiling((orderItems[newPosition] + orderItems[newPosition + 1]) / 2);
+                    orderItems[oldPosition].OrderValue = newOrderValue;
+                    if (newOrderValue == orderItems[newPosition + 1].OrderValue)
+                    {
+                        orderItems[newPosition + 1].OrderValue++;
+                        if (newPosition + 1 != orderItems.Length - 1)
+                        {
+                            while (orderItems[newPosition + 1].OrderValue == orderItems[newPosition + 2].OrderValue)
+                            {
+                                orderItems[newPosition + 2].OrderValue++;
+                                newPosition++;
+                                if (newPosition == orderItems.Length - 2)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                    
+            }
+
+           return orderItems;
+       }
+    }
+}
---
+++ TestOfWeirdAlg/WierdAlgTests.cs
@@ -0,0 +1,474 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAlgorythm;
+using System.Linq;
+
+namespace TestOfWeirdAlg
+{
+    [TestClass]
+    public class WierdAlgTests
+    {
+        #region InsertDown
+
+        [TestMethod]
+        public void TestMethodForSwappingFirstTwoElements()
+        {
+            //Arrange
+
+            #region TestData
+            var inputList1 = new List<OrderItem>()
+            {
+
+
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 5
+                },
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 10
+                }
+            };
+
+            #endregion
+
+            //Assert
+
+            var newList = OrderHelper.Reorder(inputList1, null, 0, 1).OrderBy(x => x.OrderValue).Select(x => x.OrderName).ToArray();
+
+            //Act
+
+            #region ResultData
+
+            var resultArray = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 10
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 11
+                }
+            }.Select(x => x.OrderName).ToArray();
+
+
+            #endregion
+
+            CollectionAssert.AreEqual(resultArray, newList);
+        }
+
+        [TestMethod]
+        public void TestMethodForFirstAndLastElements()
+        {
+            //Arrange
+
+            #region TestData
+            var inputList1 = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 5
+                },
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 10
+                }
+            };
+
+            #endregion
+
+            //Assert
+
+            var newList = OrderHelper.Reorder(inputList1, null, 0, inputList1.Count - 1).OrderBy(x => x.OrderValue).Select(x => x.OrderName).ToArray();
+
+            //Act
+
+            #region ResultData
+
+            var resultArray = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 10
+                },
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 11
+                }
+            }.Select(x => x.OrderName).ToArray();
+
+
+            #endregion
+
+            CollectionAssert.AreEqual(resultArray, newList);
+        }
+
+        [TestMethod]
+        public void TestMethodForInsertingToMiddleElement()
+        {
+            //Arrange
+
+            #region TestData
+            var inputList1 = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 5
+                },
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 10
+                }
+            };
+
+            #endregion
+
+            //Assert
+
+            var newList = OrderHelper.Reorder(inputList1, null, 0, 2).OrderBy(x => x.OrderValue).Select(x => x.OrderName).ToArray();
+
+            //Act
+
+            #region ResultData
+
+            var resultArray = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 10
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 11
+                }
+            }.Select(x => x.OrderName).ToArray();
+
+
+            #endregion
+
+            CollectionAssert.AreEqual(resultArray, newList);
+        }
+
+        #endregion
+
+        #region InsertUp
+        [TestMethod]
+        public void TestMethodForInsertingToMiddleElementUp()
+        {
+            //Arrange
+
+            #region TestData
+            var inputList1 = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 5
+                },
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 10
+                }
+            };
+
+            #endregion
+
+            //Assert
+
+            var newList = OrderHelper.Reorder(inputList1, null, 4, 1).OrderBy(x => x.OrderValue).Select(x => x.OrderName).ToArray();
+
+            //Act
+
+            #region ResultData
+
+            var resultArray = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 5
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 10
+                }
+            }.Select(x => x.OrderName).ToArray();
+
+
+            #endregion
+
+            CollectionAssert.AreEqual(resultArray, newList);
+
+        }
+
+        [TestMethod]
+        public void TestMethodForInsertingToTheEnd()
+        {
+            //Arrange
+
+            #region TestData
+            var inputList1 = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 5
+                },
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 10
+                }
+            };
+
+            #endregion
+
+            //Assert
+
+            var newList = OrderHelper.Reorder(inputList1, null, 3, 0).OrderBy(x => x.OrderValue).Select(x => x.OrderName).ToArray();
+
+            //Act
+
+            #region ResultData
+
+            var resultArray = new List<OrderItem>()
+            {
+                new OrderItem()
+                {
+                    OrderName = "I4",
+                    OrderValue = 5
+                },
+                new OrderItem()
+                {
+                    OrderName = "I1",
+                    OrderValue = 6
+                },
+                new OrderItem()
+                {
+                    OrderName = "I2",
+                    OrderValue = 7
+                },
+                new OrderItem()
+                {
+                    OrderName = "I3",
+                    OrderValue = 8
+                },
+                new OrderItem()
+                {
+                    OrderName = "I5",
+                    OrderValue = 9
+                },
+                new OrderItem()
+                {
+                    OrderName = "I6",
+                    OrderValue = 10
+                }
+            }.Select(x => x.OrderName).ToArray();
+
+
+            #endregion
+
+            CollectionAssert.AreEqual(resultArray, newList);
+
+        }
+
+        #endregion
+    }
+}
